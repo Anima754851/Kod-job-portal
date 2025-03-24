5220fc477ebd9107dd1dc9f8980f26fb41a7dec9
Anima754851 <Anima754851@gmail.com>
2025-03-24 12:31:19 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Component/Login";
 import Dashboard from "./Component/Dashboard";
